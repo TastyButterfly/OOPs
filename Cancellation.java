f80ea114b31dafec202146c35046bec3c652cb86
TastyButterfly <neohcl-pm23@student.tarc.edu.my>
2024-09-03 02:36:58 +0800

    added partial methods, created class files

--- Cancellation.java
+++ Cancellation.java
@@ -1,1 +1,42 @@
-public class Cancellation{}
+import java.time.LocalDateTime;
+
+public class Cancellation{
+    String cancelID;
+    Order order;
+    Product product;
+    LocalDateTime date;
+    int qty;
+    static int latestCancelID;
+    public Cancellation(String cancelID, Product product, int qty, Order order){
+        this.cancelID=cancelID;
+        this.product=product;
+        this.qty=qty;
+        this.order=order;
+        date=LocalDateTime.now();
+        product.qty+=qty;
+    }
+    public Cancellation(Order order){
+        cancelID=++latestCancelID;
+    }
+    public void changeDate(LocalDateTime date){
+        try{
+            this.date=date;
+        }catch (Exception e){
+            System.out.println("Error changing date.");
+        }
+    }
+    public void setProdandQty(Product product, int qty){
+        if(qty<=0){
+            System.out.println("Quantity must be 1 or more!");
+        }
+        try{
+            this.product=product;
+            product.qty-=this.qty;
+            product.qty+=qty;
+            this.qty=qty;
+        }catch (Exception e){
+            System.out.println("Error referencing Product object");
+        }
+    }
+
+}
