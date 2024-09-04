249f84e59116cf36eec4a26ef50ce09e4c658195
TastyButterfly <neohcl-pm23@student.tarc.edu.my>
2024-09-05 03:38:16 +0800

    deleted date.java

--- Cancellation.java
+++ Cancellation.java
@@ -1,8 +1,10 @@
+import java.time.LocalDateTime;
+
 public class Cancellation{
     private String cancelID;
     private Order order;
     private Product product;
-    public Date date;
+    private LocalDateTime date;
     private int qty;
     private static int count;
     public Cancellation(Product product, int qty, Order order){
@@ -10,17 +12,17 @@ public class Cancellation{
         this.product=product;
         this.qty=qty;
         this.order=order;
-        date=new Date();
+        date=LocalDateTime.now();
         product.qty+=qty;
     }
     public Cancellation(Order order){
         cancelID=String.format("%c%d",'C',++count);
         this.order=order;
-        date=new Date();
+        date=LocalDateTime.now();
     }
     public Cancellation(){
         cancelID=String.format("%c%d",'C',++count);
-        date=new Date();
+        date=LocalDateTime.now();
     }
     public void setOrder(Order order){
         try{
@@ -60,10 +62,51 @@ public class Cancellation{
             }
         }
     }
+    public void changeDate(int d, int m, int y){
+        date=LocalDateTime.of(y,m,d,date.getHour(),date.getMinute(),date.getSecond());
+    }
+    public void changeTime(int h, int m, int s){
+        date=LocalDateTime.of(date.getYear(),date.getMonthValue(),date.getDayOfMonth(),h,m,s);
+    }
+    public String getDateTime(){
+        return String.format("%d-%d-%d %d:%d:%d",date.getDayOfMonth(),date.getMonthValue(),date.getYear(),date.getHour(),date.getMinute(),date.getSecond());
+    }
+    public String getDate(){
+        return String.format("%d-%d-%d",date.getDayOfMonth(),date.getMonthValue(),date.getYear());
+    }
+    public String getTime(){
+        return String.format("%d:%d:%d",date.getHour(),date.getMinute(),date.getSecond());
+    }
+    public String getDMY(){
+        return String.format("%d %s %d",date.getDayOfMonth(),date.getMonth(),date.getYear());
+    }
+    public String getMY(){
+        return String.format("%s %d",date.getMonth(),date.getYear());
+    }
+    public String getDM(){
+        return String.format("%d %s",date.getDayOfMonth(),date.getMonth());
+    }
+    public String getDMValue(){
+        return String.format("%d-%d",date.getDayOfMonth(),date.getMonthValue());
+    }
+    public int getDay(){
+        return date.getDayOfMonth();
+    }
+    public int getMonthValue(){
+        return date.getMonthValue();
+    }
+    public int getYear(){
+        return date.getYear();
+    }
+    public String getMonth(){
+        return String.format("%s",date.getMonth());
+    }
+    public int getDayOfYear(){
+        return date.getDayOfYear();
+    }
     public static int addCancellation(){
         return 1;
     }
-   
     public int getCount(){
         return count;
     }
--- asgm.java
+++ asgm.java
@@ -1,6 +1,6 @@
 public class asgm {
     public static void main(String[] args) {
         Cancellation cancel=new Cancellation();
-        System.out.println(cancel.date.getDateTime());
+        System.out.println(cancel.getDateTime());
     }
 }
